a7347c8a170cde215e5ce44638c2b69830be92eb
Daniel Katzav <katzavdaniel@gmail.com>
2022-11-17 00:17:31 +0200

    progress in single Elevator logic

--- include/ElevatorClass/elevator_class.hpp
+++ include/ElevatorClass/elevator_class.hpp
@@ -6,18 +6,81 @@
 #define ELEVTAOR_SYSTEM_ELEVATOR_CLASS_HPP
 
 #include <memory>
+#include <vector>
 #include <SocketCAN.h>
+#include <map>
+
+/*
+ * Assumptions:
+ *  1. Elevator current floor determines the floor that it can also be immediately stopped
+ *  2. There is a correlation between the max acceleration and velocity of the elevator to reach a designated time to stop, begin and pass a single floor
+ * */
+
+enum ElevatorCurrentState
+{
+    STOPPED,
+    GOING_UP,
+    GOING_DOWN
+};
+
+enum ElevatorCommands
+{
+    STOP,
+    GO_UP,
+    GO_DOWN
+};
+enum ElevatorControlMode
+{
+    ELEVATOR_CONTROL,
+    REMOTE_CONTROL
+};
+
+struct ElevatorParameters
+{
+    uint32_t elevator_id_;
+
+    std::vector<int> possible_floors;
+    std::vector<int> special_floors;
+    std::map<int, std::string> floor_special_keys_map_;
+
+    int opened_doors_time_window_in_ms_ = 8000;
+
+    float max_acceleration_in_meters_per_second_square_ = 3;
+    float max_velocity_in_meters_per_second_ = 6;
+
+    int floor_in_meters_ = 3;
+    int lowest_floor_ = -2;
+    int highest_floor_ = 10;
+};
 
 class ElevatorClass
 {
 private:
     std::unique_ptr<SocketCAN> canbus_socket_;
+
+    int current_floor_ = 0;
+    int goal_floor = 0;
+    std::vector<int> floor_path;
+
+    const ElevatorParameters elevator_parameters_;
+
+    ElevatorControlMode elevator_control_mode_;
+
 public:
-    ElevatorClass(unsigned int elevator_id);
+    ElevatorClass(ElevatorParameters elevator_parameters);
 
 private:
 
 public:
+    int GetCurrentFloor() const;
+    int GetGoalFloor() const;
+    std::vector<int> GetPossibleFloors() const;
+
+    bool AddFloorToPath(int new_floor);
+    bool AddFloorToPath(int new_floor, std::string floor_special_key);
+
+    bool PerformEmergencyStop();
+    bool SetElevatorControlMode(ElevatorControlMode elevator_control_mode);
 
 };
 
